b870553e4c9bbad267aca3d374ce8f9e31978c55
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-08 16:38:29 +0100

    simple fly() method to test the this keyword

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 class Drone {
     constructor(id, name) {
-        console.log(`ID: ${id} | Name: ${name}`);
         this.id = id;
         this.name = name;
     }
+    fly() {
+        console.log(`Drone ${this.id} is flying`);
+    }
 }
-//static property
-Drone.maxHeight = 2000;
 
 let drone = new Drone('A123', 'Flyer');
 let drone2 = new Drone('B456', 'Twirl');
 
-console.log(drone.maxHeight); //undefined
-
-// console.log(typeof Drone); // function
-// console.log(drone instanceof Drone); // true
+drone.fly();
+drone2.fly();
