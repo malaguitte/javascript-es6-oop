ae071d18571c145cd5649526e8c9543cfed4ac4a
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-08 16:51:31 +0100

    playing around with the super keyword and constructors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-class Drone {
-    constructor(id, name) {
-        this._id = id;
-    }
-    get id() {
-        return this._id;
-    }
-    set id(newId) {
-        this._id = newId;
+class Vehicle {
+    constructor(licenseNumber) {
+        console.log('constructing Vehicle');
+        this.licenseNumber = licenseNumber;
     }
 }
 
-let drone = new Drone("A123");
-console.log(`Drone id: ${drone.id}`);
-drone.id = 'B456';
-console.log(`Drone id: ${drone.id}`);
+class Drone extends Vehicle {
+}
+
+class Car extends Vehicle {
+    constructor(licenseNumber) {
+        // super(); //This is required in JS
+        super(licenseNumber);
+        console.log('constructing Car');
+    }
+}
+
+let c = new Car();
+console.log(c instanceof Car); //true
+console.log(c instanceof Vehicle); //true
+console.log(c instanceof Object); //true
