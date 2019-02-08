33ea24b493687da23be0d10acaf59673101ec57d
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-08 16:36:55 +0100

    static property example

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ class Drone {
         this.name = name;
     }
 }
+//static property
+Drone.maxHeight = 2000;
 
 let drone = new Drone('A123', 'Flyer');
+let drone2 = new Drone('B456', 'Twirl');
+
+console.log(drone.maxHeight); //undefined
 
 // console.log(typeof Drone); // function
 // console.log(drone instanceof Drone); // true
