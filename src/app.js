6c84447162094c73e0e9d37ea156b3075be440a0
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-08 16:34:44 +0100

    Created Drone class with a simple constructor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Test...");
+class Drone {
+    constructor(id, name) {
+        console.log(`ID: ${id} | Name: ${name}`);
+        this.id = id;
+        this.name = name;
+    }
+}
+
+let drone = new Drone('A123', 'Flyer');
+
+// console.log(typeof Drone); // function
+// console.log(drone instanceof Drone); // true
