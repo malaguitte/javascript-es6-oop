d1f609cf4b8a6d5368b0b87dce87013257515d88
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-10 00:58:10 +0100

    Created Image class

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import {Car} from './classes/car.js';
 import {Drone} from './classes/Drone.js';
 import {data} from './data/data.js';
 import {DataService} from './services/data-service.js';
-import { Button } from './ui/button.js';
+import {Button} from './ui/button.js';
+import {Image} from './ui/image.js';
 
 
 let dataService = new DataService();
@@ -10,3 +11,6 @@ dataService.loadData(data);
 
 let button = new Button('Click me!');
 button.appendToElement($('body'));
+
+let image = new Image('images/drone.jpg');
+image.appendToElement($('body'));
