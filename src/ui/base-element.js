b1e3cf9edf4e93c8ca4387ae3365859bd6348df0
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-10 00:51:11 +0100

    fixed js effect on the button

--- src/ui/base-element.js
+++ src/ui/base-element.js
@@ -1,16 +1,25 @@
 export class BaseElement {
+
   constructor() {
     this.element = null; //jquery object
   }
+
   appendToElement(appendTo) {
     this.createElement();
     appendTo.append(this.element);
+    this.enableJS();
   }
+
   createElement() {
     let elementString = this.getElementString();
     this.element = $(elementString);
   }
+
   getElementString() {
     throw 'Please override getElementString() in BaseElement'
+  }
+
+  enableJS() {
+    componentHandler.upgradeElement(this.element[0]);
   }
 }
