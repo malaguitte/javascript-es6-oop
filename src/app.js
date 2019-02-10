966486acfaecef8a1070b928ff1b4794058ed2f8
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-10 11:30:12 +0100

    App class extending the ApplicationBase

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
-import {Car} from './classes/car.js';
-import {Drone} from './classes/Drone.js';
 import {data} from './data/data.js';
 import {DataService} from './services/data-service.js';
-import {Button} from './ui/button.js';
-import {Image} from './ui/image.js';
-import {TitleBar} from './ui/title-bar.js';
-import {DataTable} from './ui/data-table.js';
-import {GoogleMap} from './ui/google-map.js';
+import {ApplicationBase} from './framework/application-base.js';
 
+export class App extends ApplicationBase {
+  
+  constructor(title) {
+    super(title);
+    this.dataService = new DataService();
+    this.dataService.loadData(data);
+  }
+  
+}
 
-let dataService = new DataService();
-dataService.loadData(data);
+export let application = new App('Management App');
+application.show($('body'));
 
-// let headers = [];
-// headers.push('License');
-// headers.push('Make');
-// headers.push('Model');
-// headers.push('Miles');
-// let cars = dataService.cars;
-// let table = new DataTable(headers, cars);
-// table.appendToElement($('body'));
-
-let mapCenter = {lat: 40.783661, lng: -73.965883};
-let map = new GoogleMap(mapCenter, dataService.cars);
-map.appendToElement($('body'));
+// let mapCenter = {lat: 40.783661, lng: -73.965883};
+// let map = new GoogleMap(mapCenter, dataService.cars);
+// map.appendToElement($('body'));
