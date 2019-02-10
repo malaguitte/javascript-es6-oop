008db6b37a4fe39de67c3e129013a529b62ab682
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-10 12:25:42 +0100

    Created MapPage() class

---
+++ src/pages/map-page.js
@@ -0,0 +1,25 @@
+import {Page} from "../framework/page.js";
+import {GoogleMap} from "../ui/google-map.js";
+import {app} from "../app.js";
+
+export class MapPage extends Page{
+
+  constructor() {
+    super('Map');
+  }
+
+  createElement() {
+    super.createElement();
+    let mapCenter = {lat: 40.783661, lng: -73.965883};
+    let map = new GoogleMap(mapCenter, app.dataService.cars);
+    console.log(`map : ${map}`);
+    map.appendToElement(this.element);
+  }
+
+  getElementString() {
+    return `
+      <div style="margin: 20px;"><h3>Map</h3></div>
+    `;
+  }
+
+}
