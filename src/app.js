662c8fc26c05f20f7d7e3be9b2e3f22e7781015b
Anderson Malagutti <upmalagutti@gmail.com>
2019-02-10 12:25:55 +0100

    setting MapPage

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import {ApplicationBase} from './framework/application-base.js';
 import {HomePage} from './pages/home-page.js';
 import {CarsPage} from './pages/cars-page.js';
 import {DronesPage} from './pages/drones-page.js';
+import {MapPage} from './pages/map-page.js';
 
 export class App extends ApplicationBase {
   
@@ -15,7 +16,7 @@ export class App extends ApplicationBase {
     this.addRoute('Home', new HomePage(), true);
     this.addRoute('Cars', new CarsPage());
     this.addRoute('Drones', new DronesPage());
-    this.addRoute('Map', null);
+    this.addRoute('Map', new MapPage());
   }
   
 }
